54fd13c98db04a378b056abad1f213479747dbed
Ahmadreza <46781518+Ahmadrezadl@users.noreply.github.com>
2025-02-12 12:05:58 +0330

    Release v1.0.3

--- KarizmaSettings.Infrastructure/Repositories/Interfaces/IUserSettingRepository.cs
+++ KarizmaSettings.Infrastructure/Repositories/Interfaces/IUserSettingRepository.cs
@@ -7,4 +7,6 @@ public interface IUserSettingRepository : IRepository<UserSetting>
 {
     Task<List<UserSetting>> GetUserSettings(long userId, bool asNoTracking = false);
     Task<UserSetting?> GetUserSetting(long userId, string type, string key, bool asNoTracking = false);
+    public Task<T?> GetUserSettingValue<T>(long userId, string type, string key, T? defaultValue = default);
+    public Task SetUserSetting<T>(long userId, string type, string key, T value);
 }
